19c4252096d6ed2fc481f5bf5bd5c69b7093b477
SomeBadDecisions <63814959+SomeBadDecisions@users.noreply.github.com>
2022-06-06 02:05:37 +0300

    Update views.sql

--- src/views.sql
+++ src/views.sql
@@ -1,6 +1,6 @@
-create view analysis.orders as select * from production.orders;
-create view analysis.orderitems as select * from production.orderitems;
-create view analysis.orderstatuses as select * from production.orderstatuses;
-create view analysis.orderstatuslog as select * from production.orderstatuslog;
-create view analysis.products as select * from production.products;
-create view analysis.users as select * from production.users;
+create or replace view analysis.orders as select * from production.orders;
+create or replace view analysis.orderitems as select * from production.orderitems;
+create or replace view analysis.orderstatuses as select * from production.orderstatuses;
+create or replace view analysis.orderstatuslog as select * from production.orderstatuslog;
+create or replace view analysis.products as select * from production.products;
+create or replace view analysis.users as select * from production.users;
