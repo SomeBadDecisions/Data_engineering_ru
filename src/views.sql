284909ae9b0e12cfc6230a6f0f5f98784c4c384f
SomeBadDecisions <63814959+SomeBadDecisions@users.noreply.github.com>
2022-06-05 14:11:08 +0300

    Add files via upload

---
+++ src/views.sql
@@ -0,0 +1,6 @@
+create view analysis.orders as select * from production.orders;
+create view analysis.orderitems as select * from production.orderitems;
+create view analysis.orderstatuses as select * from production.orderstatuses;
+create view analysis.orderstatuslog as select * from production.orderstatuslog;
+create view analysis.products as select * from production.products;
+create view analysis.users as select * from production.users;
