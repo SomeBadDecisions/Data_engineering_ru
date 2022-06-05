4077b7c6fd6d159e02c5ec94d8aacbdc39939e31
SomeBadDecisions <63814959+SomeBadDecisions@users.noreply.github.com>
2022-06-05 14:10:53 +0300

    Delete views.sql

--- src/views.sql
+++
@@ -1,5 +0,0 @@
-create view analysis.orderitems as select * from production.orderitems;
-create view analysis.orderstatuses as select * from production.orderstatuses;
-create view analysis.orderstatuslog as select * from production.orderstatuslog;
-create view analysis.products as select * from production.products;
-create view analysis.users as select * from production.users;
