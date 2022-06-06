3fe282aab964a7a609f8f94481f226fd082d40f0
SomeBadDecisions <63814959+SomeBadDecisions@users.noreply.github.com>
2022-06-06 03:41:37 +0300

    Update tmp_rfm_frequency.sql

--- src/tmp_rfm_frequency.sql
+++ src/tmp_rfm_frequency.sql
@@ -5,13 +5,16 @@ frequency)
 SELECT * FROM (
 with pre as (
 select 
-user_id 
+t0.id as user_id 
 ,count(order_id) as metric
-from analysis.orders t1
-inner join analysis.orderstatuses t2 on t1.status = t2.id 
-									 and t2.key ='Closed'
+from analysis.users t0
+left join analysis.orders t1
+	on t0.id = t1.user_id 
+left join analysis.orderstatuses t2 
+	on t1.status = t2.id 
+	and t2.key ='Closed'
 where (extract('year' from order_ts)) >= 2021								 
-group by user_id 
+group by 1 
 ),
 
 prct as 
@@ -26,11 +29,11 @@ from pre
 select 
 user_id,
 case when metric <= (select "1" from prct) then 1
-	 when metric >= (select "1" from prct)
+	 when metric > (select "1" from prct)
 	 	and metric <=(select "2" from prct) then 2
-	 when metric >= (select "2" from prct)
+	 when metric > (select "2" from prct)
 	 	and metric <=(select "3" from prct) then 3
-	 when metric >= (select "3" from prct)
+	 when metric > (select "3" from prct)
 	 	and metric <=(select "4" from prct) then 4
 	 else 5 end as metric
 from pre 
