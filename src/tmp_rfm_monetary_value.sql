9890df4d80d59e597d6247f7dc7104ce00a1d28e
SomeBadDecisions <63814959+SomeBadDecisions@users.noreply.github.com>
2022-06-06 13:03:36 +0300

    Update tmp_rfm_monetary_value.sql

--- src/tmp_rfm_monetary_value.sql
+++ src/tmp_rfm_monetary_value.sql
@@ -2,36 +2,35 @@ INSERT INTO analysis.tmp_rfm_monetary_value (
 user_id,
 monetary_value)
 
-SELECT * FROM (
+
 with pre as (
 select 
-user_id 
-,sum(cost) as metric
-from analysis.orders t1
-inner join analysis.orderstatuses t2 on t1.status = t2.id 
-									 and t2.key ='Closed'
+t0.id as user_id
+, sum(t1.cost) as metric
+, max(t2.key) as status
+from analysis.users t0
+left join analysis.orders t1
+	on t0.id = t1.user_id 
+left join analysis.orderstatuses t2 
+	on t1.status = t2.id 
 where (extract('year' from order_ts)) >= 2021								 
-group by user_id 
+group by 1 
 ),
 
-prct as 
-(select 
-percentile_disc(0.2) within group (order by metric) as "1"
-,percentile_disc(0.4) within group (order by metric) as "2"
-,percentile_disc(0.6) within group (order by metric) as "3"
-,percentile_disc(0.8) within group (order by metric) as "4"
-from pre
+rn as (
+select 
+user_id
+,metric
+,status
+,row_number () over (order by status, metric) as rn 
+from pre 
 )
 
 select 
-user_id,
-case when metric <= (select "1" from prct) then 1
-	 when metric >= (select "1" from prct)
-	 	and metric <=(select "2" from prct) then 2
-	 when metric >= (select "2" from prct)
-	 	and metric <=(select "3" from prct) then 3
-	 when metric >= (select "3" from prct)
-	 	and metric <=(select "4" from prct) then 4
-	 else 5 end as metric
-from pre 
-) subq 
+user_id
+,case when rn <= 200 then 1
+	  when rn > 200 and rn <= 400 then 2
+	  when rn > 400 and rn <= 600 then 3
+	  when rn > 600 and rn <= 800 then 4
+	  else 5 end as metric
+from rn 
